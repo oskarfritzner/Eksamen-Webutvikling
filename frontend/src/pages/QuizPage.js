5d11c81662cbf61b28bb48c6847d5d5501ed702e
Oskar Fritzner <72510834+oskarfritzner@users.noreply.github.com>
2023-11-11 13:55:40 +0100

    Stylet quiz siden

--- frontend/src/pages/QuizPage.js
+++ frontend/src/pages/QuizPage.js
@@ -1,27 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { getQuestions, submitQuizResult } from '../services/quizServices';
-import Navbar from '../components/Navigation-Bar';
+import Navbar from "../components/Navigation-Bar";
+import { getQuestions, submitQuizResult, getParticipants } from '../services/quizServices';
 
 const QuizPage = () => {
     const [questions, setQuestions] = useState([]);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [userAnswers, setUserAnswers] = useState({});
-    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState(localStorage.getItem('quizUsername') || '');
     const [showQuiz, setShowQuiz] = useState(false);
     const [quizCompleted, setQuizCompleted] = useState(false);
     const [score, setScore] = useState(0);
+    const [leaderboard, setLeaderboard] = useState([]);
 
     useEffect(() => {
-        const fetchQuestions = async () => {
+        fetchQuestions();
+        fetchParticipants();
+    }, []);
+
+    const fetchQuestions = async () => {
+        try {
             const fetchedQuestions = await getQuestions();
             setQuestions(fetchedQuestions);
-        };
-        fetchQuestions();
-    }, []);
+        } catch (error) {
+            console.error("Failed to fetch questions:", error);
+        }
+    };
+
+    const fetchParticipants = async () => {
+        try {
+            const participants = await getParticipants();
+            const sortedParticipants = participants
+                .sort((a, b) => b.score - a.score)
+                .slice(0, 10); 
+            setLeaderboard(sortedParticipants);
+        } catch (error) {
+            console.error("Failed to fetch participants:", error);
+        }
+    };
 
     const startQuiz = () => {
         if (username) {
+            localStorage.setItem('quizUsername', username);
             setShowQuiz(true);
+            setQuizCompleted(false);
+            setCurrentQuestionIndex(0);
+            setUserAnswers({});
         } else {
             alert("Please enter your game name to start.");
         }
@@ -30,7 +53,7 @@ const QuizPage = () => {
     const handleAnswerSelect = async (answer) => {
         const question = questions[currentQuestionIndex];
         const isCorrect = answer === question.correctAnswer;
-        
+
         const newAnswers = {
             ...userAnswers,
             [question.id]: {
@@ -45,7 +68,8 @@ const QuizPage = () => {
         } else {
             setQuizCompleted(true);
             setShowQuiz(false);
-            calculateScore(newAnswers);
+            await calculateScore(newAnswers);
+            await fetchParticipants();
         }
     };
 
@@ -58,57 +82,77 @@ const QuizPage = () => {
         await submitQuizResult(username, totalScore);
     };
 
-    return (
-        <div>
-            <Navbar />
-        <div className="bg-black text-white min-h-screen">
-            <div className="container mx-auto p-4 flex justify-center items-center min-h-screen">
-                <div className="w-full max-w-md">
-                    <h2 className="text-2xl font-bold mb-4 text-center">F1 Quiz Time!</h2>
-                    {!showQuiz && !quizCompleted && (
-                        <div className="flex flex-col items-center">
-                            <input 
-                                type="text" 
-                                placeholder="Enter your game name" 
-                                value={username} 
-                                onChange={(e) => setUsername(e.target.value)} 
-                                className="border border-gray-300 rounded p-2 mb-4 bg-white text-black"
-                            />
-                            <button onClick={startQuiz} className="bg-f1-red hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                                Play
-                            </button>
-                        </div>
-                    )}
-                    {showQuiz && (
-                        <div className="text-center">
-                            <p className="mb-2">Question {currentQuestionIndex + 1} of {questions.length}</p>
-                            <p className="font-semibold mb-4">{questions[currentQuestionIndex].query}</p>
+        // Reset quizCompleted when username changes
+        useEffect(() => {
+            setQuizCompleted(false);
+        }, [username]);
+
+        return (
+            <div className="bg-black text-white min-h-screen">
+                <Navbar />
+                <div className="container mx-auto p-4 flex flex-col items-center min-h-screen">
+                    {/* Quiz Game Section */}
+                    <div className="w-full max-w-lg mt-44 mb-10">
+                        <h2 className="text-2xl font-bold mb-4 text-center">F1 Quiz Time!</h2>
+                        {!showQuiz && (
                             <div className="flex flex-col items-center">
-                                {questions[currentQuestionIndex].answers.map((answer, index) => (
-                                    <button 
-                                        key={answer} 
-                                        onClick={() => handleAnswerSelect(answer)}
-                                        className="bg-gray-100 hover:bg-gray-200 text-gray-800 py-2 px-4 my-1 rounded w-full"
-                                    >
-                                        {answer}
-                                    </button>
-                                ))}
+                                <input 
+                                    type="text" 
+                                    placeholder="Enter your game name" 
+                                    value={username} 
+                                    onChange={(e) => setUsername(e.target.value)} 
+                                    className="border border-gray-300 rounded p-2 mb-4 text-black"
+                                />
+                                <button onClick={startQuiz} className="bg-f1-red hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                    {quizCompleted ? "Play Again" : "Play"}
+                                </button>
                             </div>
+                        )}
+                        {showQuiz && (
+                            <div className="text-center">
+                                <p className="mb-2">Question {currentQuestionIndex + 1} of {questions.length}</p>
+                                <p className="font-semibold mb-4">{questions[currentQuestionIndex].query}</p>
+                                <div className="flex flex-col items-center">
+                                    {questions[currentQuestionIndex].answers.map((answer, index) => (
+                                        <button 
+                                            key={answer} 
+                                            onClick={() => handleAnswerSelect(answer)}
+                                            className="bg-gray-100 hover:bg-gray-200 text-gray-800 py-2 px-4 my-1 rounded w-full"
+                                        >
+                                            {['A', 'B', 'C', 'D'][index]}. {answer}
+                                        </button>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+                        {quizCompleted && (
+                            <div className="text-center">
+                                <p className="text-lg font-semibold">
+                                    Thank you for participating, {username}! You got {score}/10 points.
+                                </p>
+                            </div>
+                        )}
+                    </div>
+        
+                    {/* Leaderboard Section */}
+                    <div className="w-full max-w-xl mb-10">
+                        <h3 className="text-xl font-bold mb-4">Leaderboard</h3>
+                        <div className="bg-gray-800 p-4 rounded">
+                            {leaderboard.map((participant, index) => (
+                                <div key={index} className="flex justify-between border-b border-gray-700 py-2">
+                                    <span>{participant.username}</span>
+                                    <span style={{ color: participant.username === localStorage.getItem('quizUsername') ? '#f10606' : 'white' }}>
+                                        {participant.score}
+                                    </span>
+                                </div>
+                            ))}
                         </div>
-                    )}
-                    {quizCompleted && (
-                        <div className="text-center">
-                            <p className="text-lg font-semibold">
-                                Thank you for participating, {username}! You got {score}/10 points.
-                            </p>
-                        </div>
-                    )}
+                    </div>
                 </div>
             </div>
-        </div>
-
-        </div>
-    );
+        );
+        
+    
 };
 
 export default QuizPage;
--- frontend/src/services/quizServices.js
+++ frontend/src/services/quizServices.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = 'https://localhost:7093';
 
-export const getQuestions = async () => {
+const getQuestions = async () => {
     try {
         const response = await axios.get(`${API_URL}/Questions`);
         return response.data;
@@ -12,7 +12,7 @@ export const getQuestions = async () => {
     }
 };
 
-export const submitQuizResult = async (username, score) => {
+const submitQuizResult = async (username, score) => {
     try {
         const response = await axios.post(`${API_URL}/Participants`, {
             username,
@@ -24,3 +24,16 @@ export const submitQuizResult = async (username, score) => {
         throw error;
     }
 };
+
+const getParticipants = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/Participants`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching participants:", error);
+        throw error;
+    }
+};
+
+
+export { getQuestions, submitQuizResult, getParticipants };
