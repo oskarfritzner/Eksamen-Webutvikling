2020c7e24fe5f2cd3bfdee06aff335af7f2a26a1
Oskar Fritzner <72510834+oskarfritzner@users.noreply.github.com>
2023-11-11 13:57:15 +0100

    stylet participating message

--- frontend/src/pages/QuizPage.js
+++ frontend/src/pages/QuizPage.js
@@ -126,7 +126,7 @@ const QuizPage = () => {
                             </div>
                         )}
                         {quizCompleted && (
-                            <div className="text-center">
+                            <div className="text-center mt-8">
                                 <p className="text-lg font-semibold">
                                     Thank you for participating, {username}! You got {score}/10 points.
                                 </p>
