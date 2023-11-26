7dc928f1a6a891074cab73e9aea0883f90d21a24
Oskar Fritzner <72510834+oskarfritzner@users.noreply.github.com>
2023-11-26 18:15:40 +0100

    Add Driver1 and Driver2 to Team Model

---
+++ backend/Migrations/20231126171117_UpdateTeamModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTeamModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Driver1",
+                table: "Teams",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Driver2",
+                table: "Teams",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Driver1",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "Driver2",
+                table: "Teams");
+        }
+    }
+}
--- backend/Models/Team.cs
+++ backend/Models/Team.cs
@@ -5,5 +5,7 @@ namespace backend.Models
         public int Id { get; set; }
         public string Manufacturer { get; set; }
         public string Image { get; set; }
+        public string? Driver1 { get; set; }
+        public string? Driver2 { get; set; }
     }
 }
